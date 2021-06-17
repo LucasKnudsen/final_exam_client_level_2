d2c582858c8be1207f2ae93bc5f14bb4f229e54a
lucas <laksteelhouse@gmail.com>
2021-06-17 13:45:00 +0200

    Filters dublicates from category menu

--- react-app/cypress/integration/userCanFilterBetweenCategories.feature.js
+++ react-app/cypress/integration/userCanFilterBetweenCategories.feature.js
@@ -5,15 +5,15 @@ describe('User can filter between course categories', () => {
 
   it('is expected to display 6 category tabs', () => {
     cy.get('[data-cy=nav-bar]').find('li').should('have.length', 6)
-    cy.get('[data-cy=nav-bar]').find('li').eq(0).should('contain', 'Workshops')
-    cy.get('[data-cy=nav-bar]').find('li').eq(1).should('contain', 'Testing')
-    cy.get('[data-cy=nav-bar]').find('li').eq(2).should('contain', 'Vue')
+    cy.get('[data-cy=nav-bar]').find('li').eq(0).should('contain', 'workshops')
+    cy.get('[data-cy=nav-bar]').find('li').eq(1).should('contain', 'testing')
+    cy.get('[data-cy=nav-bar]').find('li').eq(2).should('contain', 'vue')
     cy.get('[data-cy=nav-bar]')
       .find('li')
       .eq(3)
-      .should('contain', 'Productivity')
-    cy.get('[data-cy=nav-bar]').find('li').eq(4).should('contain', 'Node')
-    cy.get('[data-cy=nav-bar]').find('li').eq(5).should('contain', 'React')
+      .should('contain', 'productivity')
+    cy.get('[data-cy=nav-bar]').find('li').eq(4).should('contain', 'node')
+    cy.get('[data-cy=nav-bar]').find('li').eq(5).should('contain', 'react')
   })
 
   it('is expected to filter the courses based on selected category', () => {
--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { courses } from '../data/courses'
 
 const Header = () => {
+  const categoriesArray = (list) => {
+    let categories = []
+    list.forEach((course) => {
+      if (categories.indexOf(course.category) === -1) {
+        categories.push(course.category)
+      }
+    })
+    return categories
+  }
+
+  const menuList = categoriesArray(courses).map((category) => {
+    return <li className='capitalized bold'>{category}</li>
+  })
+
   return (
     <>
       <h1>Course list</h1>
+      <ul data-cy='nav-bar'>{menuList}</ul>
     </>
   )
 }
